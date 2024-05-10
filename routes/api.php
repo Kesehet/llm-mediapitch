aaa9d4c2b259b7b3b08b0854af37989378e91098
Kesehet <hamood.siddiqui@gmail.com>
2024-05-10 15:17:11 +0530

    updated time format

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Models\Task;
+use Carbon\Carbon;
 
 
 
@@ -25,9 +26,18 @@ Route::get('/tasks/response/{uuid}', [TaskController::class, 'getResponse']);
 
 Route::patch('/tasks/{task}', [TaskController::class, 'updateTask']);
 
+
 Route::get("/all-tasks", function () {
-    return Task::where('status', 'pending')->orwhere('status', 'processing')->get();
+    $tasks = Task::where('status', 'pending')->orWhere('status', 'processing')->get();
+
+    // Add Unix timestamp to each task object
+    foreach ($tasks as $task) {
+        $task->unix_timestamp = Carbon::parse($task->created_at)->timestamp;
+    }
+
+    return $tasks;
 });
+
 
 
 
